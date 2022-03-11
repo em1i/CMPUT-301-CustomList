530597ea5d52b7109b227c2aa2d47bf3e717eed0
“Em” <98789620+em1i@users.noreply.github.com>
2022-03-11 14:14:09 -0700

    test case

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -61,6 +61,8 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void addCity(City city){
+        //add here to fix the test
+        cities.add(city);
         return;
     }
 }
